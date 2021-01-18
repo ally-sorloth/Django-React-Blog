86b5d28cf57a3a6faf11a64cd96203b17ce70fc2
ally-sorloth <allysorloth@gmail.com>
2021-01-18 23:33:58 +0300

    add models

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post, PostView, Like, Comment
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(PostView)
+admin.site.register(Like)
+admin.site.register(Comment)
+
+
+
+
 
 # Register your models here.
