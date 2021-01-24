60fdd2ee7e97bc77736f0cabd86f8b2d33eb00f7
ally-sorloth <allysorloth@gmail.com>
2021-01-24 23:09:40 +0300

    add serializers

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post, PostView, Comment, Like
+
+
+# def post_list(request):
+#     qs = Post.objects.filter(status='p')
+#     context = {
+#         "object_list": qs
+#     }
+#     return render(request, "blog/post_list.html", context)
+
 
 # Create your views here.
