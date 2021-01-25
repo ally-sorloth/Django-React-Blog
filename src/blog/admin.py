0d13b83c56d6b4978dbf5b9bd9dce25a2e9154b1
ally-sorloth <allysorloth@gmail.com>
2021-01-25 23:03:12 +0300

    added admin content

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import Category, Post, PostView, Like, Comment
 
-admin.site.register(Category)
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'author')
+    search_fields = ('title',)
+    list_display_links = ('title', 'author')
+    # prepopulated_fields = {
+    #     "slug": ("title",),
+    # }
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'time_stamp')
+
+admin.site.register(Post, PostAdmin)
+# admin.site.register(Category)
 admin.site.register(PostView)
 admin.site.register(Like)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
+
 
 
 
